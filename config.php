4318e60605838a7c2ae2769db141fb642061317c
Light Dev Server <lightbot@lightrains.com>
2016-07-08 12:48:42 +0530

    Remove config

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-ini_set('memory_limit', '-1');
-mysql_connect('138.201.119.126','metromat_GoMovie','~DeFacil4*GasolinA#') or die(mysql_error());
-mysql_select_db('metromat_movieworld');
-
-?>

03972797f59c5a96f9eb7cdae786a98ba8524063
root <nikhil@lightrains.com>
2016-07-08 09:11:16 +0200

    Adding config

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 
 ini_set('memory_limit', '-1');
-mysql_connect('localhost','remote','myR3mote122') or die(mysql_error());
-mysql_select_db('metrodata_data');
+mysql_connect('138.201.119.126','metromat_GoMovie','~DeFacil4*GasolinA#') or die(mysql_error());
+mysql_select_db('metromat_movieworld');
 
 ?>

44ec9eb69c88b52b4ce158cc70e97f5efcf4aba6
Light Server <light@dev.lightrains.com>
2017-02-15 12:56:47 +0530

    Artist image count

---
+++ artist-image-count.php
@@ -0,0 +1,49 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+<?php
+require 'functions.php';
+
+$data = artist_image_count();
+
+$output = "";
+$index = 1;
+
+foreach ($data as $key => $value) {
+	foreach ($value as $k => $v) {
+		if ($v != "") {
+
+			$c = ceil($key / 50);
+
+			$link = '<ul class="list-group">';
+
+			for ($i = 1; $i <= $c; $i++) {
+
+				$link .= '<li class="list-group-item">http://old.metromatinee.com/api/MetroService/artist-image.php?q=&id=' . $v . '&page=' . $i . '&count=50</li>';
+			}
+
+			$link .= "</ul>";
+
+			$output .= '<div class="panel panel-default">
+					  <div class="panel-heading">
+					  <span class="badge">' . $key . '</span>
+					  <span class="badge">' . $c . '</span>
+					  ' . $index . ' # ' . $v . '</div>
+					  <div class="panel-body">
+					    ' . $link . '
+					  </div>
+					</div>';
+
+			$index++;
+		}
+	}
+}
+
+?>
+
+<div class="container">
+	<div class="col-md-6">
+		<?php echo $output; ?>
+	</div>
+</div>
--- artist-image.php
+++ artist-image.php
@@ -1,19 +1,10 @@
 <?php
-require('functions.php');
+require 'functions.php';
 
-//Pager
 $page = $_GET['page'];
 $itemsPerPage = isset($_GET['count']) ? $_GET['count'] : 1;
-
-//Id as parameter
-//$id = $_GET['id'];
-//print_r($id);
-
-$data = getArtistImagenew($page,$itemsPerPage);
-//print_r($data);
+$id = $_GET['id'];
+$data = getArtistImage($id, $page, $itemsPerPage);
 echo json_encode($data);
-	// echo "<pre>";
-	// print_r($data);
-	// echo "</pre>";
 
 ?>

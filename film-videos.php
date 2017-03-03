bd77791cda518a3e67cc1895d5e89f2be8f96b13
Light Server <light@dev.lightrains.com>
2017-03-03 12:17:03 +0530

    Added film id

--- film-videos.php
+++ film-videos.php
@@ -1,21 +1,21 @@
 <?php
-require('functions.php');
+require 'functions.php';
 
 $page = $_GET['page'];
 $itemsPerPage = isset($_GET['count']) ? $_GET['count'] : 1;
 
 $cat = $_GET['category'];
-//$id = $_GET['id'];
+$id = $_GET['id'];
 
-if($cat == "trailers"){
-	$data = getFilmTrailers($cat,$page,$itemsPerPage);
+if ($cat == "trailers") {
+	$data = getFilmTrailers($cat, $id, $page, $itemsPerPage);
 }
 
-if($cat == "clips"){
-	$data = getFilmClips($cat,$page,$itemsPerPage);
+if ($cat == "clips") {
+	$data = getFilmClips($cat, $page, $itemsPerPage);
 }
 echo json_encode($data);
-	// echo "<pre>";
-	// print_r($data);
-	// echo "</pre>";
+// echo "<pre>";
+// print_r($data);
+// echo "</pre>";
 ?>

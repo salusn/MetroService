2360b3430592d052d13cb8e52dd22c1d359b6003
Light Server <light@dev.lightrains.com>
2017-03-06 13:55:44 +0530

    corrected artist clips API

--- clips.php
+++ clips.php
@@ -1,14 +1,15 @@
 <?php
-require('functions.php');
+require 'functions.php';
 //Pager
 $page = $_GET['page'];
 $itemsPerPage = isset($_GET['count']) ? $_GET['count'] : 1;
+$id = $_GET['id'];
 
-$data = getArtistClipsByFilmID($page,$itemsPerPage);
+$data = getArtistClipsByFilmID($id, $page, $itemsPerPage);
 //print_r($data);
 echo json_encode($data);
-	// echo "<pre>";
-	// print_r($data);
-	// echo "</pre>";
+// echo "<pre>";
+// print_r($data);
+// echo "</pre>";
 
 ?>

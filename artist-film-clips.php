d4eabf19777cc78dff3ffc33f13b492bcb890fa8
Light Server <light@dev.lightrains.com>
2017-03-07 15:50:37 +0530

    Added artist id

--- artist-film-clips.php
+++ artist-film-clips.php
@@ -1,15 +1,16 @@
 <?php
-require('functions.php');
+require 'functions.php';
 
 //Pager
 $page = $_GET['page'];
 $itemsPerPage = isset($_GET['count']) ? $_GET['count'] : 1;
+$id = $_GET['id'];
 
-$data = getArtistClipsByArtistID($page,$itemsPerPage);
+$data = getArtistClipsByArtistID($id, $page, $itemsPerPage);
 //print_r($data);
 echo json_encode($data);
-	// echo "<pre>";
-	// print_r($data);
-	// echo "</pre>";
+// echo "<pre>";
+// print_r($data);
+// echo "</pre>";
 
 ?>
